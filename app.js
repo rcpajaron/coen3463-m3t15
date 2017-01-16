64ffb2291f0dacb7f17101ae0bdd1952db5eab0c
rcpajaron <roemarpajaron@gmail.com>
2017-01-16 17:45:01 +0800

    initial commit

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ MongoClient.connect(mdbUrl, function(err, database) {
             contact_number: req.body.contact_number,
             birthdate: req.body.birthdate,
             section: req.body.section,
-            createdate: moment().format('LLL')
+            updatedate: moment().format('LLL')
           
         };
         studentCollection.updateOne({ _id: new ObjectId(studentId)},{$set: datasave}, function(err, student) {
